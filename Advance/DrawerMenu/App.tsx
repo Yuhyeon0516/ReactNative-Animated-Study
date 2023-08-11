a0851cec8274d486af8b7febf89c582991ad61be
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-11 16:38:35 +0900

    created drawer menu using animated

---
+++ Advance/DrawerMenu/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import DrawerMenu from './src/DrawerMenu';
+
+function App(): JSX.Element {
+  return <DrawerMenu />;
+}
+
+export default App;
