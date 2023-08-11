2d25f0a5e929a09568ae4442c4ad89138044e3e8
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-11 12:25:06 +0900

    studyed timing animation 2

--- src/AnimatedComponent.tsx
+++ src/AnimatedComponent.tsx
@@ -2,12 +2,33 @@ import {View, Text, Button, Animated} from 'react-native';
 import React, {useRef} from 'react';
 
 // opacity 1 -> 0 timing animation
+// export default function AnimatedComponent() {
+//   const opacityAnim = useRef(new Animated.Value(1)).current;
+
+//   function onButtonPress() {
+//     Animated.timing(opacityAnim, {
+//       toValue: 0,
+//       useNativeDriver: true,
+//     }).start();
+//   }
+
+//   return (
+//     <>
+//       <Button title="!!!!!!!" onPress={onButtonPress} />
+//       <Animated.Text style={{fontSize: 70, opacity: opacityAnim}}>
+//         🍎
+//       </Animated.Text>
+//     </>
+//   );
+// }
+
+// 왼쪽에서 오른쪽으로 이동하는 X가 변화하는 애니메이션
 export default function AnimatedComponent() {
-  const opacityAnim = useRef(new Animated.Value(1)).current;
+  const translateXAnim = useRef(new Animated.Value(-100)).current;
 
   function onButtonPress() {
-    Animated.timing(opacityAnim, {
-      toValue: 0,
+    Animated.timing(translateXAnim, {
+      toValue: 100,
       useNativeDriver: true,
     }).start();
   }
@@ -15,7 +36,8 @@ export default function AnimatedComponent() {
   return (
     <>
       <Button title="!!!!!!!" onPress={onButtonPress} />
-      <Animated.Text style={{fontSize: 70, opacity: opacityAnim}}>
+      <Animated.Text
+        style={{fontSize: 70, transform: [{translateX: translateXAnim}]}}>
         🍎
       </Animated.Text>
     </>
