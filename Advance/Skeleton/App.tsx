a3868a0447637d4c5d7aac8450d33c59282fc832
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-14 15:10:31 +0900

    created skeleton using animated

---
+++ Advance/Skeleton/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Skeleton from './src/Skeleton';
+
+function App(): JSX.Element {
+  return <Skeleton />;
+}
+
+export default App;
